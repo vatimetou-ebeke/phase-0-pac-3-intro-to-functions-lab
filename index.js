eb7a4789a0128e34f300f19e7954470e32fdb461
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-27 14:45:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -14,18 +14,21 @@ function shout(string) {
        console.log(HELLO.toLowerCase());
    }
 
-   function sayHiToHeadphonedRoommate(string){
-       if( string.toLowerCase()){
-       return "I can\'t hear you!"  
-   } 
-   else if(string.toUpperCase()){
-       
-       return"YES INDEED";
-   }  
-
-    else if(string==="Let's have dinner together!" ) {
-       return"I would love to!";
-   }
   
-   
+   function sayHiToHeadphonedRoommate(string){
+
+    if( string.toLowerCase()==string){
+
+        return "I can't hear you!"  
+ 
+    }
+     else if(string.toUpperCase()==string){  
+ 
+        return"YES INDEED!";
+        
+    }  
+    else if(string==="Let's have dinner together!" ) {
+ 
+        return"I would love to!";
+    }
 }
